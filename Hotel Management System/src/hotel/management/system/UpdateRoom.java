b7488d8ab88dc5a1816312c701a45a681ec087d3
Houtarou Oreki <oreki.houtarou01@gmail.com>
2025-10-18 23:07:28 +0800

    updated java

--- Hotel Management System/src/hotel/management/system/UpdateRoom.java
+++ Hotel Management System/src/hotel/management/system/UpdateRoom.java
@@ -181,3 +181,11 @@ PreparedStatement pst = null;
 	}
 
 }
+
+/*
+  To change this license header, choose License Headers in Project Properties.
+  To change this template file, choose Tools | Templates
+  and open the template in the editor.
+
+	This is my project, BSIT
+ */
